04029f4948359059a37114058919cd7648d21020
cin-tie <csizegod@gmail.com>
2026-03-27 23:41:01 +0300

    Service listener writed

--- src/main/java/com/cintie/messenger/network/ServerListener.java
+++ src/main/java/com/cintie/messenger/network/ServerListener.java
@@ -1,4 +1,39 @@
 package com.cintie.messenger.network;
 
-public class ServerListener {
+import com.cintie.messenger.service.MessageService;
+import com.sun.jndi.ldap.Connection;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListener implements Runnable{
+
+    private final int port;
+    private final ConnectionManager connectionManager;
+    private final MessageService messageService;
+
+    public ServerListener(int port, ConnectionManager connectionManager, MessageService messageService){
+        this.port = port;
+        this.connectionManager = connectionManager;
+        this.messageService = messageService;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Listening on port: " + port);
+
+            while (true){
+                Socket socket = serverSocket.accept();
+
+                ClientConnection connection = new ClientConnection(socket, connectionManager, messageService);
+                connectionManager.addConnection(connection);
+
+                new Thread(connection).start();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
