ed61bf0065021b5f1ef73441e8bd23e4871b9f9c
cin-tie <csizegod@gmail.com>
2026-07-11 15:11:31 +0300

    Writed signature service

--- src/main/java/com/cintie/messenger/crypto/SignatureService.java
+++ src/main/java/com/cintie/messenger/crypto/SignatureService.java
@@ -3,6 +3,8 @@ package com.cintie.messenger.crypto;
 import com.cintie.messenger.protocol.Packet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 
 public class SignatureService {
@@ -44,5 +46,49 @@ public class SignatureService {
         return identityManager.verify(dataToVerify, signature, senderPublicKey);
     }
 
+    // Check peerId and publicKey
+    public boolean verifyPeerId(String peerId, PublicKey publicKey) throws Exception{
+        byte[] rawPublicKey = publicKey.getEncoded();
+        byte[] hash = computeSha256(rawPublicKey);
+        String computedPeerId = bytesToHex(hash);
+        return computedPeerId.equals(peerId);
+    }
 
+    // Get SHA-256 hash
+    private byte[] computeSha256(byte[] data){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(data);
+        } catch (NoSuchAlgorithmException e){
+            throw new RuntimeException("SHA-256 not available", e);
+        }
+    }
+
+    // Convert bytes to hex
+    private String bytesToHex(byte[] bytes){
+        StringBuilder hexString = new StringBuilder();
+        for(byte b: bytes){
+            String hex = Integer.toHexString(0xff & b);
+            if(hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+
+    private Packet copyWithoutSignature(Packet original){
+        Packet copy = new Packet();
+        copy.setVersion(original.getVersion());
+        copy.setPacketType(original.getPacketType());
+        copy.setPacketId(original.getPacketId());
+        copy.setSenderId(original.getSenderId());
+        copy.setRecipientId(original.getRecipientId());
+        copy.setTimestamp(original.getTimestamp());
+        copy.setNonce(original.getNonce());
+        copy.setPayload(original.getPayload());
+        // signature null
+        return copy;
+    }
 }
